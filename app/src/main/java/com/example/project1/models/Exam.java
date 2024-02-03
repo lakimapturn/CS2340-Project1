4d74a22d3728db32fa1a4c7592e46063e98e2305
FinallyComplete <mihirnbalsara@gmail.com>
2024-02-03 16:06:43 -0500

    exams implementation

--- app/src/main/java/com/example/project1/models/Exam.java
+++ app/src/main/java/com/example/project1/models/Exam.java
@@ -1,27 +1,42 @@
 package com.example.project1.models;
 
-import java.time.LocalDateTime;
+import android.os.Parcel;
+import android.os.Parcelable;
 
-public class Exam {
-    private String courseName;
-    private LocalDateTime dateTime;
+import androidx.annotation.NonNull;
+
+import java.util.Date;
+
+public class Exam implements Parcelable {
+    private Date dateTime;
     private String location;
 
     public Exam() {
-        this(null, null, null);
+        this(null, null);
     }
 
-    public Exam(String courseName, LocalDateTime dateTime, String location) {
-        this.courseName = courseName;
+    public Exam(Date dateTime, String location) {
         this.dateTime = dateTime;
         this.location = location;
     }
 
-    public String getCourseName() {
-        return courseName;
+    protected Exam(Parcel in) {
+        location = in.readString();
     }
 
-    public LocalDateTime getData() {
+    public static final Creator<Exam> CREATOR = new Creator<Exam>() {
+        @Override
+        public Exam createFromParcel(Parcel in) {
+            return new Exam(in);
+        }
+
+        @Override
+        public Exam[] newArray(int size) {
+            return new Exam[size];
+        }
+    };
+
+    public Date getDateTime() {
         return dateTime;
     }
 
@@ -29,15 +44,22 @@ public class Exam {
         return location;
     }
 
-    public void setCourseName(String courseName) {
-        this.courseName = courseName;
-    }
-
-    public void setData(LocalDateTime dateTime) {
+    public void setDateTime(Date dateTime) {
         this.dateTime = dateTime;
     }
 
     public void setLocation(String location) {
         this.location = location;
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(@NonNull Parcel dest, int flags) {
+        dest.writeLong(this.dateTime != null ? this.dateTime.getTime() : -1);
+        dest.writeString(this.location);
     }
 }
