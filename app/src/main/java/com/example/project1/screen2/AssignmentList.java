388504483970a4c95277a015840d632086d1ac06
Laksh Makhija <76146478+lakimapturn@users.noreply.github.com>
2024-02-03 13:32:41 -0500

    completed assignment page

--- app/src/main/java/com/example/project1/screen2/AssignmentList.java
+++ app/src/main/java/com/example/project1/screen2/AssignmentList.java
@@ -1,23 +1,20 @@
 package com.example.project1.screen2;
 
 import android.os.Bundle;
-import android.os.PersistableBundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
-import androidx.navigation.Navigation;
 import androidx.navigation.fragment.NavHostFragment;
 
-import com.example.project1.MainActivity;
 import com.example.project1.R;
 import com.example.project1.components.ListAdapter;
 import com.example.project1.models.Assignment;
@@ -27,8 +24,15 @@ import java.util.Date;
 
 public class AssignmentList extends Fragment {
 
-    public ArrayList<Assignment> list = new ArrayList<>();
-    private ListAdapter<Assignment> listAdapter;
+    public static ArrayList<Assignment> assignments = new ArrayList<>();
+    private com.example.project1.components.ListAdapter<Assignment> listAdapter;
+
+    public AssignmentList() {
+//        assignments.add(new Assignment("Example1", new Date(), "12345"));
+//        assignments.add(new Assignment("Example2", new Date(), "12345"));
+//        assignments.add(new Assignment("Example3", new Date(), "12345"));
+//        assignments.add(new Assignment("Example4", new Date(), "12345"));
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -39,7 +43,7 @@ public class AssignmentList extends Fragment {
         textView.setText("Assignments");
 
         Button addButton = AssignmentListLayout.findViewById(R.id.add);
-        textView.setText("Add Assignment");
+        addButton.setText("Add Assignment");
 
         return AssignmentListLayout;
     }
@@ -47,25 +51,33 @@ public class AssignmentList extends Fragment {
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        list.add(new Assignment("Example1", new Date(), "12345"));
-        list.add(new Assignment("Example2", new Date(), "12345"));
-        list.add(new Assignment("Example3", new Date(), "12345"));
-        list.add(new Assignment("Example4", new Date(), "12345"));
+        Assignment a = AssignmentListArgs.fromBundle(getArguments()).getData();
+        int index = AssignmentListArgs.fromBundle(getArguments()).getIndex();
+
+        if(index != -1) {
+            assignments.set(index, a);
+        } else if (a != null) {
+            assignments.add(a);
+        }
 
         ListView listView = view.findViewById(R.id.list);
-        listAdapter = new ListAdapter<>(view.getContext(), list);
+        listAdapter = new ListAdapter<>(view.getContext(), assignments);
         listView.setAdapter(listAdapter);
 
-        view.findViewById(R.id.edit).setOnClickListener((View v) -> {
-            // TODO: how to check which list item was clicked
-            AssignmentListDirections.ActionAssignmentList2ToAddEditAssignments2 action =
-                    AssignmentListDirections.actionAssignmentList2ToAddEditAssignments2(null);
-            NavHostFragment.findNavController(AssignmentList.this).navigate(action);
+        listAdapter.setOnItemClickListener(new ListAdapter.OnItemClickListenerInterface() {
+            @Override
+            public void onItemClick(int position) {
+                System.out.println(position);
+
+                AssignmentListDirections.ActionAssignmentList2ToAddEditAssignments2 action =
+                        AssignmentListDirections.actionAssignmentList2ToAddEditAssignments2(position);
+                NavHostFragment.findNavController(AssignmentList.this).navigate(action);
+            }
         });
 
         view.findViewById(R.id.add).setOnClickListener((View v) -> {
             AssignmentListDirections.ActionAssignmentList2ToAddEditAssignments2 action =
-                    AssignmentListDirections.actionAssignmentList2ToAddEditAssignments2(null);
+                    AssignmentListDirections.actionAssignmentList2ToAddEditAssignments2(-1);
             NavHostFragment.findNavController(AssignmentList.this).navigate(action);
         });
 
